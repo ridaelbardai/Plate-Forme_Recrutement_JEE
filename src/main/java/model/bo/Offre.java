6cd7483d0f409dcacfc513474c2a55ea0044b508
Rida El Bardai <rb.99@hotmail.fr>
2022-03-28 21:33:46 +0000

    bootstrap

---
+++ src/main/java/model/bo/Offre.java
@@ -0,0 +1,109 @@
+package model.bo;
+
+
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "offre")
+public class Offre {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String titre;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date;
+	
+	private String description;
+
+	@ManyToOne
+    @JoinColumn(name = "fk_user")
+	private User createur;
+	
+	@ManyToOne
+    @JoinColumn(name = "fk_type")
+	private TypeOffre type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public TypeOffre getType() {
+		return type;
+	}
+
+
+	public User getCreateur() {
+		return createur;
+	}
+
+	public void setCreateur(User createur) {
+		this.createur = createur;
+	}
+
+	public void setType(TypeOffre type) {
+		this.type = type;
+	}
+
+	public Offre() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Offre(String titre, String description) {
+		super();
+		this.titre = titre;
+		this.description = description;
+		this.date = new Date();
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Offre [id=" + id + ", titre=" + titre + ", description=" + description + ", createur=" + createur
+				+ ", type=" + type + "]";
+	}
+
+
+
+}
---
+++ src/main/java/model/dao/DAOOffre.java
@@ -0,0 +1,86 @@
+package model.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import model.bo.Offre;
+import model.bo.User;
+import model.service.HibernateUtil;
+
+public class DAOOffre {
+	public void insererOffre(Offre o) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			
+			session.save(o);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+
+	public List<Offre> getListOffres() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		return session.createQuery("FROM Offre").list();
+
+	}
+
+	public Offre getOffreByID(int id) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		return session.load(Offre.class, id);
+	}
+	
+	
+	public void updateOffre(Offre o) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+
+			session.update(o);
+			// commit transaction
+			session.flush();
+			transaction.commit();
+			session.close();
+
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.getMessage();
+		}
+
+	}
+	
+	
+	public void supprimerOffre(Offre o) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+
+			session.delete(o);
+			System.out.println("Dans supprimerOffre");
+			// commit transaction
+			session.flush();
+			transaction.commit();
+			session.close();
+		} catch (Exception e) {
+//			if (transaction != null) {
+//				transaction.rollback();
+//			}
+			e.getMessage();
+		}
+	}
+}
